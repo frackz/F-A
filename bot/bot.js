7b71b9a9aef9b30dd42f0f3706b95c5858cb6d77
frede <65610875+dkfrede@users.noreply.github.com>
2022-07-30 12:09:14 +0200

    status

--- bot/bot.js
+++ bot/bot.js
@@ -16,6 +16,13 @@ module.exports = class BotManager extends Discord.Client {
     }
 
     run() {
+        this.on('ready', () => {
+            console.log("hey")
+            this.user.setPresence({
+                activities: [{ name: `licensing`, type: Discord.ActivityType.Watching }],
+                status: 'dnd',
+            });
+        })
         new EventLoader(this).load()
         new CommandLoader(this).load()
 
